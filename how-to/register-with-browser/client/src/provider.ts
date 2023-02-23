5b1664b139df4e46bdbba252b03776073a218019
Robert Shaw <r.shaw1@yahoo.com>
2023-02-23 00:16:32 +0000

    focus on customize-workspace how-to

--- how-to/register-with-browser/client/src/provider.ts
+++
@@ -1,8 +0,0 @@
-import { init as bootstrap } from "./bootstrapper";
-import { init as initialisePlatform } from "./platform";
-
-window.addEventListener("DOMContentLoaded", async () => {
-	const platform = fin.Platform.getCurrentSync();
-	await platform.once("platform-api-ready", async () => bootstrap());
-	await initialisePlatform();
-});
